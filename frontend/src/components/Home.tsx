2399a9d8d5a7dab763be52d2dc4d0409b7dfd72e
Loc Hoang <hpkhanhloc@gmail.com>
2021-03-09 16:51:46 +0200

    feat(frontend): brieft information in home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import {
+  Container,
+  Paper,
+  Box,
+  Typography,
+  CardContent,
+} from "@material-ui/core";
 
 const Home: React.FC = () => {
-    return (
-        <h1>Home</h1>
-    )
-}
+  return (
+    <Container>
+      <Paper>
+        <Box p={2}>
+          <CardContent>
+            <Typography variant="h5">
+              Web application for Aiven interview assignment
+            </Typography>
+            <Typography variant="body1">
+              Frontend: React, TypeScript, Material-UI
+            </Typography>
+            <Typography variant="body1">
+              Backend: FastAPI, Python deployed on Cloud Run GCP
+            </Typography>
+            <Typography variant="body1">
+              Funtions: Support filter by cloud provider, region, sort by
+              distance. Futher more it is possible to sort by click on each
+              property in table header. Navigate to Search view for more detail.
+            </Typography>
+            <Typography variant="body1">
+              UI: Show/hide drawer in top left app bar. Support table
+              pagination.
+            </Typography>
+            <Typography variant="body1">
+              CI/CD: use Github action for firebase deploy
+            </Typography>
+            <Typography variant="body1">Author: Loc Hoang - 3.2021</Typography>
+          </CardContent>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
